13cdca66b1ebb785e474b1e9c6e19b039ed57aa0
Herbert <herbert@kreationsbyran.se>
2020-06-05 16:27:57 +0200

    optional Fluent

--- src/Injector/FluentExtensionInjector.php
+++ src/Injector/FluentExtensionInjector.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace NorthCreationAgency\SSExtras;
+
+use function class_exists;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\LiteralField;
 use SilverStripe\ORM\ArrayList;
@@ -15,56 +18,58 @@ use TractorCow\Fluent\Model\Locale;
  * Date: 2019-12-11
  * Time: 22:19
  */
-
-class FluentExtensionInjector extends FluentExtension
-{
-
-
-    public function ActiveInLocale($locale){
-        $class = get_class($this->owner);
-        $schema = DataObject::getSchema();
-        $localisedTable = $this->owner->getLocalisedTable($schema->tableName($class));
-        $item=DB::query("SELECT * FROM ".$localisedTable." WHERE Locale='".$locale."' AND RecordID=".$this->owner->ID)->first();
-        if ($item){
-            return true;
-        }
-    }
-
-    /**
-     * @param FieldList $fields
-     */
-    public function updateCMSFields(FieldList $fields)
+if (class_exists("TractorCow\Fluent\Extension\FluentExtension")) {
+    class FluentExtensionInjector extends FluentExtension
     {
-        parent::updateCMSFields($fields);
-        $currentLocale = Locale::getCurrentLocale();
-        $badgeClasses = ['badge', 'fluent-badge'];
-        if ($currentLocale->getIsDefault() && $this->owner->ActiveInLocale($currentLocale->Locale)) {
-            // Current locale should always show a "default" or green state badge
-            $badgeClasses[] = 'fluent-badge--default';
-            $tooltip = _t(__CLASS__ . '.BadgeDefault', 'Default locale');
-        } elseif ($this->owner->ActiveInLocale($currentLocale->Locale)) {
-            // If the object has been localised in the current locale, show a "localised" state
-            $badgeClasses[] = 'fluent-badge--localised';
-            $tooltip = _t(__CLASS__ . '.BadgeInvisible', 'Localised in {locale}', [
-                'locale' => $currentLocale->getTitle(),
-            ]);
-        } else {
-            // Otherwise the state is that it hasn't yet been localised in the current locale, so is "invisible"
-            $badgeClasses[] = 'fluent-badge--invisible';
-            $tooltip = _t(__CLASS__ . '.BadgeLocalised', '{type} is not visible in this locale', [
-                'type' => $this->owner->i18n_singular_name(),
-            ]);
+
+
+        public function ActiveInLocale($locale)
+        {
+            $class = get_class($this->owner);
+            $schema = DataObject::getSchema();
+            $localisedTable = $this->owner->getLocalisedTable($schema->tableName($class));
+            $item = DB::query("SELECT * FROM " . $localisedTable . " WHERE Locale='" . $locale . "' AND RecordID=" . $this->owner->ID)->first();
+            if ($item) {
+                return true;
+            }
         }
 
-        $fields->unshift(LiteralField::create(
-            'HTMLFragment',
-            sprintf(
-                '<span class="%s" title="%s" style="margin-left:0;">%s</span>',
-                implode(' ', $badgeClasses),
-                $tooltip,
-                $this->owner->ID?$this->owner->getSourceLocale()->getBadgeLabel():$currentLocale->Locale
-            )));
+        /**
+         * @param FieldList $fields
+         */
+        public function updateCMSFields(FieldList $fields)
+        {
+            parent::updateCMSFields($fields);
+            $currentLocale = Locale::getCurrentLocale();
+            $badgeClasses = ['badge', 'fluent-badge'];
+            if ($currentLocale->getIsDefault() && $this->owner->ActiveInLocale($currentLocale->Locale)) {
+                // Current locale should always show a "default" or green state badge
+                $badgeClasses[] = 'fluent-badge--default';
+                $tooltip = _t(__CLASS__ . '.BadgeDefault', 'Default locale');
+            } elseif ($this->owner->ActiveInLocale($currentLocale->Locale)) {
+                // If the object has been localised in the current locale, show a "localised" state
+                $badgeClasses[] = 'fluent-badge--localised';
+                $tooltip = _t(__CLASS__ . '.BadgeInvisible', 'Localised in {locale}', [
+                    'locale' => $currentLocale->getTitle(),
+                ]);
+            } else {
+                // Otherwise the state is that it hasn't yet been localised in the current locale, so is "invisible"
+                $badgeClasses[] = 'fluent-badge--invisible';
+                $tooltip = _t(__CLASS__ . '.BadgeLocalised', '{type} is not visible in this locale', [
+                    'type' => $this->owner->i18n_singular_name(),
+                ]);
+            }
+
+            $fields->unshift(LiteralField::create(
+                'HTMLFragment',
+                sprintf(
+                    '<span class="%s" title="%s" style="margin-left:0;">%s</span>',
+                    implode(' ', $badgeClasses),
+                    $tooltip,
+                    $this->owner->ID ? $this->owner->getSourceLocale()->getBadgeLabel() : $currentLocale->Locale
+                )));
+        }
+
+
     }
-
-
 }
